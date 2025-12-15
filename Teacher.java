a62646ed81459e3f9c621e18f4d758079cc4b9ff
chitchimy9-lang <chitchimy9@gmail.com>
2025-12-15 11:34:57 +0700

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -2,76 +2,20 @@ package co.istad.generic;
 
 import java.math.BigDecimal;
 
-
 public class Teacher extends Person {
+
     private BigDecimal salary;
-
-    @Override
-    public String getName() {
-        return super.getName();
-    }
-
-    public Teacher(String name, String gender, Float height) {
-        super(name, gender, height);
-    }
-
-    @Override
-    public void setName(String name) {
-        super.setName(name);
-    }
-
-    @Override
-    public String getGender() {
-        return super.getGender();
-    }
-
-    @Override
-    public void setGender(String gender) {
-        super.setGender(gender);
-    }
-
-    @Override
-    public Float getHeight() {
-        return super.getHeight();
-    }
-
-    @Override
-    public void setHeight(Float height) {
-        super.setHeight(height);
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
 
     public Teacher(String name, String gender, Float height, BigDecimal salary) {
         super(name, gender, height);
         this.salary = salary;
     }
 
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
-
-
-    public BigDecimal getSalary(){
+    public BigDecimal getSalary() {
         return salary;
     }
-    public void setSalary(BigDecimal salary){
+
+    public void setSalary(BigDecimal salary) {
         this.salary = salary;
     }
-    }
-
+}
